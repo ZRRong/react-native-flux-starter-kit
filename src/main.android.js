95f4ea5782f587f9bfc4cde492702af29b863b87
Mingdong Luo <mdluo@nsmss.com>
2015-12-04 21:53:55 +0800

    Initial commit

---
+++ src/main.android.js
@@ -0,0 +1,16 @@
+/**
+ * React Native Flux Starter Kit
+ * (C) 2015 Mingdong Luo (https://github.com/mdluo) | MIT License
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './components/App';
+
+class Root extends App {
+  static defaultProps = {
+    ...App.defaultProps,
+    instructions: 'Shake or press menu button for dev menu',
+  }
+}
+
+AppRegistry.registerComponent('App', () => Root);
---
+++ src/main.ios.js
@@ -0,0 +1,16 @@
+/**
+ * React Native Flux Starter Kit
+ * (C) 2015 Mingdong Luo (https://github.com/mdluo) | MIT License
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './components/App';
+
+class Root extends App {
+  static defaultProps = {
+    ...App.defaultProps,
+    instructions: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+  }
+}
+
+AppRegistry.registerComponent('App', () => Root);
